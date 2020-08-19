7d3448bbb0f4a9b5fe887d854aaf3b73873ffead
Steven Hecking <shecking1115@gmail.com>
2020-08-19 16:40:59 -0400

    Adding proof of concept dice rolling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,11 +9,21 @@ import SignIn from '../SignIn/SignIn'
 import SignOut from '../SignOut/SignOut'
 import ChangePassword from '../ChangePassword/ChangePassword'
 
+import one from '../../assets/one.png'
+import two from '../../assets/two.png'
+import three from '../../assets/three.png'
+import four from '../../assets/four.png'
+import five from '../../assets/five.png'
+import six from '../../assets/six.png'
+
 class App extends Component {
   constructor () {
     super()
 
     this.state = {
+      numberOfDice: null,
+      rolls: [],
+      rollSum: null,
       user: null,
       msgAlerts: []
     }
@@ -25,6 +35,20 @@ class App extends Component {
 
   msgAlert = ({ heading, message, variant }) => {
     this.setState({ msgAlerts: [...this.state.msgAlerts, { heading, message, variant }] })
+  }
+
+  diceRoll = numberOfDice => {
+    const rolls = []
+    let rollSum = 0
+    for (let i = 0; i < numberOfDice; i++) {
+      rolls[i] = Math.floor(Math.random() * 6) + 1
+      rollSum += rolls[i]
+    }
+    this.setState({
+      numberOfDice,
+      rolls,
+      rollSum
+    })
   }
 
   render () {
@@ -55,8 +79,42 @@ class App extends Component {
             <ChangePassword msgAlert={this.msgAlert} user={user} />
           )} />
         </main>
+        <div className="buttons">
+          {[1, 2, 3, 4, 5].map(number => {
+            const text = number === 1 ? 'die' : 'dice'
+            return (
+              <button
+                key={number}
+                onClick={() => this.diceRoll(number)}
+                className='button'
+              >
+                {number} {text}
+              </button>
+            )
+          })}
+          <br/>
+          {
+            this.state.rolls.map((roll, index) => <DiceImage roll={roll} key={index} />)
+          }
+        </div>
       </Fragment>
     )
+  }
+}
+
+const DiceImage = ({ roll }) => {
+  if (roll === 1) {
+    return <img className='dice-image' src={one} alt='1' />
+  } else if (roll === 2) {
+    return <img className='dice-image' src={two} alt='2' />
+  } else if (roll === 3) {
+    return <img className='dice-image' src={three} alt='3' />
+  } else if (roll === 4) {
+    return <img className='dice-image' src={four} alt='4' />
+  } else if (roll === 5) {
+    return <img className='dice-image' src={five} alt='5' />
+  } else if (roll === 6) {
+    return <img className='dice-image' src={six} alt='6' />
   }
 }
 
